8d35e6734739c1c3e423d5d57a2ca958a5f38150
Luke <luke.manongsong.cics@ust.edu.ph>
2023-01-27 15:24:06 +0800

    some changes

--- routes/student.js
+++ routes/student.js
@@ -3,19 +3,19 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/home', (req, res) => {
-    res.render();
+    res.render('student/home');
 });
 
 router.get('/activities', (req, res) => {
-    
+    res.render('student/activities');
 });
 
 router.get('/capstone-projects', (req, res) => {
-    
+    res.render('student/capstone-projects');
 });
 
 router.get('/group', (req, res) => {
-    
+    res.render('student/group');
 });
 
 module.exports = router;
